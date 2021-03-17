2f35da12756220bea001c0a9c5420414891f8256
Leo Cheung <leo@cloutjam.com>
2021-03-17 01:10:23 -0400

    Part 1 tutorial done

---
+++ layouts/default.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <TheHeader />
+    <main class="main-content">
+      <Nuxt />
+    </main>
+  </div>
+</template>
+
+<script>
+import TheHeader from '@/components/TheHeader/TheHeader'
+
+export default {
+  components: {
+    TheHeader,
+  },
+}
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+body {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+}
+
+.main-content {
+  margin-top: 4.5rem;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,58 @@
+<template>
+  <section id="posts">
+    <PostPreview
+      v-for="post in posts"
+      :id="post.id"
+      :key="post.id"
+      :title="post.title"
+      :excerpt="post.previewText"
+      :thumbnail-image="post.thumbnailUrl"
+    />
+  </section>
+</template>
+
+<script>
+import PostPreview from '@/components/Blog/PostPreview'
+
+export default {
+  components: {
+    PostPreview,
+  },
+  data() {
+    return {
+      posts: [
+        {
+          title: 'A New Beginning',
+          previewText: "This will be awesome, don't miss it!",
+          thumbnailUrl:
+            'https://images.unsplash.com/photo-1615910388452-fdad95ebb038?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80',
+          id: 'a-new-beginning',
+        },
+        {
+          title: 'A Second Beginning',
+          previewText: "This will be awesome, don't miss it!",
+          thumbnailUrl:
+            'https://images.unsplash.com/photo-1615729947596-a598e5de0ab3?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80',
+          id: 'a-second-beginning',
+        },
+      ],
+    }
+  },
+}
+</script>
+
+<style scoped>
+#posts {
+  padding-top: 2rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+
+@media (min-width: 35rem) {
+  #posts {
+    flex-direction: row;
+  }
+}
+</style>
